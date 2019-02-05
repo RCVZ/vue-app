b5a2e3ab644c64e8670f47501b0294dde1092f8a
RCVZ <robinvz17@gmail.com>
2019-02-05 21:59:55 +0100

    FontAwesomeIcon

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faSearch,
+  faAlignJustify,
+  faListUl,
+  faPlusSquare,
+  faTh
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faSearch, faAlignJustify, faListUl, faPlusSquare, faTh);
+Vue.component("FontAwesomeIcon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
